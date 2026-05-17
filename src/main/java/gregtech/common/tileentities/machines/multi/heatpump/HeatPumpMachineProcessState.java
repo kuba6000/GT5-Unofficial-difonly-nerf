00a94ed6c9004a4b358b53562b9af87ba3512dd3
Pxx500 <pbartulik@gmail.com>
2026-05-17 20:35:57 +0200

    115. persist heatpump outputs

--- src/main/java/gregtech/common/tileentities/machines/multi/heatpump/HeatPumpMachineProcessState.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/heatpump/HeatPumpMachineProcessState.java
@@ -3,26 +3,39 @@ package gregtech.common.tileentities.machines.multi.heatpump;
 import net.minecraft.nbt.NBTTagCompound;
 
 import gregtech.api.metatileentity.implementations.integratedfluid.machine.IFNMachineProcess;
+import gregtech.api.metatileentity.implementations.integratedfluid.machine.IFNMachineOutputBuffer;
 
 public final class HeatPumpMachineProcessState {
 
     public static final String NBT_KEY = "ifnMachineProcess";
+    public static final String OUTPUT_BUFFER_NBT_KEY = "ifnMachineOutputBuffer";
 
     private IFNMachineProcess process = IFNMachineProcess.create();
+    private IFNMachineOutputBuffer outputBuffer = new IFNMachineOutputBuffer();
 
     public IFNMachineProcess process() {
         return process;
+    }
+
+    public IFNMachineOutputBuffer outputBuffer() {
+        return outputBuffer;
     }
 
     public void save(NBTTagCompound tag) {
         NBTTagCompound processTag = new NBTTagCompound();
         process.writeToNBT(processTag);
         tag.setTag(NBT_KEY, processTag);
+        NBTTagCompound outputBufferTag = new NBTTagCompound();
+        outputBuffer.writeToNBT(outputBufferTag);
+        tag.setTag(OUTPUT_BUFFER_NBT_KEY, outputBufferTag);
     }
 
     public void load(NBTTagCompound tag) {
         if (tag != null && tag.hasKey(NBT_KEY)) {
             process = IFNMachineProcess.readFromNBT(tag.getCompoundTag(NBT_KEY));
+        }
+        if (tag != null && tag.hasKey(OUTPUT_BUFFER_NBT_KEY)) {
+            outputBuffer = IFNMachineOutputBuffer.readFromNBT(tag.getCompoundTag(OUTPUT_BUFFER_NBT_KEY));
         }
     }
 }
--- src/test/java/gregtech/common/tileentities/machines/multi/HeatPumpMachineProcessStateTest.java
+++ src/test/java/gregtech/common/tileentities/machines/multi/HeatPumpMachineProcessStateTest.java
@@ -1,11 +1,14 @@
 package gregtech.common.tileentities.machines.multi;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import net.minecraft.nbt.NBTTagCompound;
 import org.junit.jupiter.api.Test;
 
+import gregtech.api.metatileentity.implementations.integratedfluid.machine.IFNBatchState;
 import gregtech.api.metatileentity.implementations.integratedfluid.machine.IFNMachineProcess;
+import gregtech.api.metatileentity.implementations.integratedfluid.machine.IFNMachineOutputBuffer;
 import gregtech.common.tileentities.machines.multi.heatpump.HeatPumpMachineProcessState;
 
 class HeatPumpMachineProcessStateTest {
@@ -16,16 +19,24 @@ class HeatPumpMachineProcessStateTest {
         process.setEnabled(false);
         NBTTagCompound processTag = new NBTTagCompound();
         process.writeToNBT(processTag);
+        IFNMachineOutputBuffer outputBuffer = new IFNMachineOutputBuffer();
+        outputBuffer.addPendingOutput("red", IFNBatchState.of("water", 1_000L, 300.0d, 1.0f));
+        NBTTagCompound outputBufferTag = new NBTTagCompound();
+        outputBuffer.writeToNBT(outputBufferTag);
 
         NBTTagCompound inputTag = new NBTTagCompound();
         inputTag.setTag("ifnMachineProcess", processTag);
+        inputTag.setTag("ifnMachineOutputBuffer", outputBufferTag);
         HeatPumpMachineProcessState state = new HeatPumpMachineProcessState();
         state.load(inputTag);
 
         NBTTagCompound outputTag = new NBTTagCompound();
         state.save(outputTag);
         IFNMachineProcess restored = IFNMachineProcess.readFromNBT(outputTag.getCompoundTag("ifnMachineProcess"));
+        IFNMachineOutputBuffer restoredOutputBuffer = IFNMachineOutputBuffer
+            .readFromNBT(outputTag.getCompoundTag("ifnMachineOutputBuffer"));
 
         assertFalse(restored.isEnabled());
+        assertEquals(1_000L, restoredOutputBuffer.pendingOutput("red").amountQ());
     }
 }
